c8db78982f3667b92a8afa58b25dd53864c664b3
B.Saikiran <157346742+SaikiranBashaboina@users.noreply.github.com>
2024-01-25 11:22:42 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,17 @@
+class first
+{
+   public static void main(String args[])
+   {
+    int b1=12,b2=23,b3;
+    System.out.println("Before Swap");
+    System.out.println("b1 is "+b1);
+    System.out.println("b2 is "+b2);
+    b3=b2;
+    b2=b1;
+    b1=b3;
+    System.out.println("After Swap");
+    System.out.println("b1 is "+b1);
+    System.out.println("b2 is "+b2);
+
+   }
+}

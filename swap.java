8d666dc607036290276586fc772b031a526b5940
SandeepRampha <r.sandeep2421@gmail.com>
2024-01-25 11:28:48 +0530

    Edit by sai in sandy Pc

--- swap.java
+++ swap.java
@@ -2,6 +2,7 @@ class first
 {
    public static void main(String args[])
    {
+    //hello sai this is sandeep
     int b1=12,b2=23,b3;
     System.out.println("Before Swap");
     System.out.println("b1 is "+b1);

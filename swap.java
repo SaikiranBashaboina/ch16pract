0434e3c2a15cf35e4e90cf05b2568d59d96050bd
mithintipaulsamuel <mithintisamuel99@gmail.com>
2024-01-27 15:59:41 +0530

    Edited by Sam

--- swap.java
+++ swap.java
@@ -1,15 +1,18 @@
+import java.util.Scanner;
 class first
 {
    public static void main(String args[])
    {
     //hello sai this is sandeep
-    int b1=12,b2=23,b3;
+     Scanner s=new Scanner(System.in);
+     System.out.println("enter any two numbers : ");
+    int b1=s.nextInt(),b2=s.nextInt(),b3=0;
     System.out.println("Before Swap");
     System.out.println("b1 is "+b1);
     System.out.println("b2 is "+b2);
-    b3=b2;
-    b2=b1;
-    b1=b3;
+    b2=b1+b2;
+    b1=b2-b1;
+    b2=b2-b1;
     System.out.println("After Swap");
     System.out.println("b1 is "+b1);
     System.out.println("b2 is "+b2);
